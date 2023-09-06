1276571185b9f309d43309e3486cd7e904b7ec77
bm4706 <bm4706@naver.com>
2023-09-06 16:29:31 +0900

    시작

---
+++ todohw/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for todohw project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todohw.settings')
+
+application = get_asgi_application()
