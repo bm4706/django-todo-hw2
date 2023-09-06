a7edf5e238e6750e2fcf7ad7f89e7fde9687e83f
bm4706 <bm4706@naver.com>
2023-09-06 17:01:20 +0900

    index 설정1버전

--- Todo/views.py
+++ Todo/views.py
@@ -1,3 +1,21 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+
+
+def index(request):
+    if request.method == "GET":
+        todos = Todo.objects.all()  # read
+        # print(todos)
+        context = {
+            "todos": todos
+        }
+        return render(request, "todo/index.html", context)
+    else:
+        return HttpResponse("타당하지않은", status=405)
+
+
+def create(request):
+    return HttpResponse("")
+
 
 # Create your views here.
