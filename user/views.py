98b1c0fb68c84133a04769f251e73654826b4f77
bm4706 <bm4706@naver.com>
2023-09-07 18:24:42 +0900

    db에서 비밀번호 못보게 하는거 구현

--- user/views.py
+++ user/views.py
@@ -11,7 +11,9 @@ def signup(request):
     if request.method == "POST":
         username = request.POST.get("username")
         password = request.POST.get("password")
-        User.objects.create(username=username, password=password)
+        # User.objects.create(username=username, password=password)
+        # user는 creat_user 써줘야함 안그러면 db를 통해 비밀번호 알아낼수있음
+        User.objects.create_user(username=username, password=password)
         # a.save()
         return redirect("/todo/index/")
     elif request.method == "GET":
